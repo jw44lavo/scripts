4527870be0b610912ec451495d9824a9a668a3e5
jw44lavo <j.wurz@gmx.de>
2019-11-13 14:17:57 +0100

    python script to extract the first few reads from a fasta/fastq/...

--- get_few_reads.py
+++ get_few_reads.py
@@ -1,22 +1,22 @@
 from Bio import SeqIO
+import sys 
 
-number_of_reads = 10
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+number_of_reads = int(sys.argv[3])
+current_read_count = int(sys.argv[4]) -1
 
-input_file = "input.fastq"
-
-output_file = "output.fastq"
 output_handle = open(output_file, "w")
 
 with open(input_file) as input_handle:
     for i, record in enumerate(SeqIO.parse(input_handle, "fastq")):
         
-        if i <= number_of_reads:
+        if i > current_read_count and i <= current_read_count + number_of_reads:
             SeqIO.write(record, output_handle, "fastq")
 
-        if i > number_of_reads:
+        elif i > current_read_count + number_of_reads:
             break
                 
 output_handle.close()
-
 
 print("done")
